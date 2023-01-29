8961f5fef9f5c3751b9e16581e037b3d8d494369
Cole Stephenson <colestephenson94@gmail.com>
2023-01-29 14:30:44 -0700

    Fetch proprietary API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/Layout/Layout'
 
-export default function Home() {
+export default function Home({movies}) {
+
   return (
     <div>
       <Layout>
       </Layout>
     </div> 
   )
+}
+
+export const getServerSideProps = async() => {
+  const resData = await fetch('https://gbla-api.vercel.app/movies')
+  const data = await resData.json();
+
+    return {
+      props: {
+        movies: data
+    }
+  } 
 }
