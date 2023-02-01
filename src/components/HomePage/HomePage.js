4250e8bd3d52327f6b102b9285d5579f2e4cb76f
Cole Stephenson <colestephenson94@gmail.com>
2023-01-31 19:47:25 -0700

    Add secondary intro component and smooth scrolling

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import css from '../../styles/HomePage.module.css'
+import React from 'react';
+import css from '../../styles/HomePage.module.css';
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div className={css.homepage}>
-        <img className={css.handsome_mf} src="https://avatars.githubusercontent.com/u/102827145?v=4"/>
-        <h2>Hi! My name is Cole.</h2>
+        <section className={css.intro_section}>
+            <img className={css.handsome_mf} src="https://avatars.githubusercontent.com/u/102827145?v=4"/>
+            <Link href="#secondary_section" scroll={false}><h2 className={css.intro_scroll_link}>Hi! My name is Cole. I love to code. &#8595;</h2></Link>
+        </section>
+        <section id="secondary_section" className={css.intro_section2}>
+            <p>I also am a giant movie nerd, and I'm planning to watch 150 movies in 2023.</p>
+        </section>
     </div>
   )
 }
